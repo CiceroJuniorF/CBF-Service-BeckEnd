ebc43341033c78e7cd04a70197464cde5e73e983
cicerojuniorf <cicerojunior.cont@gmail.com>
2017-07-11 12:42:30 -0300

    Melhorando semantica, trocando nome da chave Id.

--- src/main/java/br/com/teste/entites/Usuario.java
+++ src/main/java/br/com/teste/entites/Usuario.java
@@ -10,28 +10,28 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 import br.com.teste.enums.Acesso;
 
-
 @MappedSuperclass
-@XmlRootElement	
+@XmlRootElement
 public class Usuario {
-	//Atributos
+	// Atributos
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id_funcionario")
-	private Integer idFuncionario;
+	@Column(name = "id_usuario")
+	private Integer idUsuario;
 	private String nome;
 	private String email;
 	private String password;
-	
+
 	@ManyToOne
 	private Endereco endereco;
+
+	@Column(name = "nivel_acesso")
 	private Acesso nivelAcesso;
-	
-	
-	
+
 	@Deprecated
-	public Usuario() {}
-	
+	public Usuario() {
+	}
+
 	public Usuario(String nome, String email, String password, Endereco endereco, Acesso nivelAcesso) {
 		this.nome = nome;
 		this.email = email;
@@ -39,58 +39,62 @@ public class Usuario {
 		this.endereco = endereco;
 		this.nivelAcesso = nivelAcesso;
 	}
+
 	public Usuario(String nome, String email, String password, Acesso nivelAcesso) {
 		this.nome = nome;
 		this.email = email;
-		this.password = password;		
+		this.password = password;
 		this.nivelAcesso = nivelAcesso;
 	}
-	
-	
-	//GGAS
-	
-	public Integer getIdFuncionario() {
-		return idFuncionario;
+
+	// GGAS
+
+	public Integer getIdUsuario() {
+		return idUsuario;
 	}
 
-	public void setIdFuncionario(Integer idFuncionario) {
-		this.idFuncionario = idFuncionario;
+	public void setIdUsuario(Integer idUsuario) {
+		this.idUsuario = idUsuario;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public Endereco getEndereco() {
 		return endereco;
 	}
+
 	public void setEndereco(Endereco endereco) {
 		this.endereco = endereco;
 	}
+
 	public Acesso getNivelAcesso() {
 		return nivelAcesso;
 	}
+
 	public void setNivelAcesso(Acesso nivelAcesso) {
 		this.nivelAcesso = nivelAcesso;
 	}
-	
-	
-	
-	
-	
-	
-	
+
 }
