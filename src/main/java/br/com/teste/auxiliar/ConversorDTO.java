247e6a5ebc1bb8eaee094b465203e7df175f702f
cicerojuniorf <cicerojunior.cont@gmail.com>
2017-07-20 14:07:19 -0300

    Adicionado métodos de converter as entidades em DTO para vendedor

--- src/main/java/br/com/teste/auxiliar/ConversorDTO.java
+++ src/main/java/br/com/teste/auxiliar/ConversorDTO.java
@@ -1,36 +1,59 @@
 package br.com.teste.auxiliar;
 
-
-
 import java.util.ArrayList;
 import java.util.List;
 
 import br.com.teste.dto.AjudanteDTO;
+import br.com.teste.dto.VendedorDTO;
 import br.com.teste.entites.Ajudante;
+import br.com.teste.entites.Vendedor;
 
 public class ConversorDTO {
-	
-	//Ajudante
-	private List<AjudanteDTO> listDTO = new ArrayList<>();
-	
+
+	// Ajudante
 	public AjudanteDTO converteAjudante(Ajudante ajudante) {
-		
-		return new AjudanteDTO(ajudante.getIdUsuario(),ajudante.getNome(), ajudante.getEmail(),ajudante.getPassword(),
-				ajudante.getEndereco(),
-				ajudante.getSalarioBase(),
-				ajudante.getSalarioComissao());
+
+		return new AjudanteDTO(ajudante.getIdUsuario(), ajudante.getNome(), ajudante.getEmail(), ajudante.getPassword(),
+				ajudante.getSalarioBase(), ajudante.getSalarioComissao());
 	}
-	
+
 	public List<AjudanteDTO> coverteListaAjudante(List<Ajudante> lista) {
-		
-		for (Ajudante ajudante : lista) {			
-			System.out.println("AQUIIII--->>>"+ajudante.toString());
-			this.listDTO.add(this.converteAjudante(ajudante));
-		}	
-		
+		List<AjudanteDTO> listDTO = new ArrayList<>();
+		for (Ajudante ajudante : lista) {
+			listDTO.add(this.converteAjudante(ajudante));
+		}
+
 		return listDTO;
 	}
-	
-	//Ajudante
-	
+
+	// Ajudante
+
+	// -----------------------------------------------------------------------------------------------------------------//
+	// -----------------------------------------------------------------------------------------------------------------//
+	// -----------------------------------------------------------------------------------------------------------------//
+	// -----------------------------------------------------------------------------------------------------------------//
+
+	// Vendedor
+	public VendedorDTO converteVendedor(Vendedor Vendedor) {
+
+		return new VendedorDTO(Vendedor.getIdUsuario(), Vendedor.getNome(), Vendedor.getEmail(), Vendedor.getPassword(),
+				Vendedor.getSalarioBase(), Vendedor.getSalarioComissao());
+	}
+
+	public List<VendedorDTO> coverteListaVendedor(List<Vendedor> lista) {
+		List<VendedorDTO> listDTO = new ArrayList<>();
+		for (Vendedor Vendedor : lista) {
+			listDTO.add(this.converteVendedor(Vendedor));
+		}
+
+		return listDTO;
+	}
+
+	// Vendedor
+
+	// -----------------------------------------------------------------------------------------------------------------//
+	// -----------------------------------------------------------------------------------------------------------------//
+	// -----------------------------------------------------------------------------------------------------------------//
+	// -----------------------------------------------------------------------------------------------------------------//
+
 }
